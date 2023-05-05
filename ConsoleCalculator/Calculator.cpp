a606e77a7d21ded2d6ca8c2617349a68ae88f1ca
Dejorn Byfield <dejornbyfield78@gmail.com>
2023-05-05 14:37:19 -0500

    updated

--- ConsoleCalculator/Calculator.cpp
+++ ConsoleCalculator/Calculator.cpp
@@ -9,7 +9,9 @@ double Calculator::Calculate(double x, char oper, double y)
 	case '-':
 		return x - y; 
 	case '*':
-		return x * y; 
+		return x * y;
+	case '/':
+		return x / y; 
 	default:
 		return 0.0; 
 	}
